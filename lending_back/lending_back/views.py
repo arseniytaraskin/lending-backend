61c163096661d87278b6be15cb514c826b196c8f
Arseniy <taraskin.arseniy@gmail.com>
2024-12-09 09:24:34 +0500

    1. Добавлена функция для отправки сообщения в Telegram сотрудникам, о том что пришла новая заявка в таблицу.

--- lending_back/lending_back/views.py
+++ lending_back/lending_back/views.py
@@ -18,6 +18,22 @@ SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, os.getenv("SERVICE_ACCOUNT_FILE"))
 
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram_notification(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        print(f"Ошибка Telegram API: {response.status_code}, {response.text}")
+
+
 class SubmitApplicationView(APIView):
     def post(self, request):
         try:
